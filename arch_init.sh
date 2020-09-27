d5b789dbadd6d60fa4c482c6c0496bc4e0b780d0
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2020-09-27 15:55:45 +0100

    init arch linux

---
+++ arch_init.sh
@@ -0,0 +1,15 @@
+# Update
+yay -Syu
+
+# Install Deepin
+yay -S deepin-extra
+
+# Install
+yay -S opera
+yay -S blender
+yay -S krita
+yay -S atom
+
+# Install Maya
+gpg --keyserver pool.sks-keyservers.net --recv-keys 8048643BA2C840F4F92A195FF54984BFA16C640F
+yay -S maya
