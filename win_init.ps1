1999d01b8c78f505b56665e8fd503b2c343f7f76
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2021-09-04 19:29:16 +0100

    Added Win init
    Windows init use Scoop

---
+++ win_init.ps1
@@ -0,0 +1,22 @@
+﻿## Install Scoop
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+iwr -useb get.scoop.sh | iex
+
+scoop bucket add extras
+
+## Install Software
+
+# Internet
+scoop install opera
+scoop intsall discord
+
+# Programming
+scoop install gitkraken
+scoop install pycharm
+scoop inatall atom
+
+# Game
+scoop install steam
+
+# System
+scoop intsall 7zip
