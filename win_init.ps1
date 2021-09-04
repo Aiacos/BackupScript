8e8dd3bcf4191403fbfc12b6b0913aa59cb0b89b
Lorenzo <uni.lorenzo.a@gmail.com>
2021-09-04 20:38:11 +0200

    Update with atom apkg

--- win_init.ps1
+++ win_init.ps1
@@ -10,13 +10,26 @@ scoop bucket add extras
 scoop install opera
 scoop intsall discord
 
-# Programming
-scoop install gitkraken
-scoop install pycharm
-scoop inatall atom
-
 # Game
 scoop install steam
 
 # System
 scoop intsall 7zip
+
+# Programming
+scoop install gitkraken
+scoop install pycharm
+scoop inatall atom
+
+# Atom Extension
+apm install teletype
+apm install file-icons
+apm install atom-file-icons
+apm install script
+apm install tool-bar
+apm install tool-bar-atom
+apm install python-tools
+apm install atom-beautify
+apm install atom-ide-debugger-python
+apm install atom-ide-ui
+apm install ide-python
