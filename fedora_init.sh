ac22c1aea275fe066d9ad92440e52f890125e2e4
Lorenzo <uni.lorenzo.a@gmail.com>
2022-09-03 17:25:09 +0100

    fedora ssh

--- fedora_init.sh
+++ fedora_init.sh
@@ -6,4 +6,9 @@ sudo dnf -y --refresh upgrade
 pip3 install --user gnome-extensions-cli
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,close'
 
+# Configure SSH
+sudo dnf install openssh-server -y
+sudo systemctl enable sshd
+sudo systemctl start sshd
+
 # Install Apps
