cc4c38823a8e679e2c013924d20f8af42de5dff1
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2023-12-17 15:21:01 +0100

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -101,6 +101,8 @@ sudo apt install geany -y
 sudo apt install cmatrix -y
 
 # Add Flatpack
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 sudo flatpak install flathub com.anydesk.Anydesk -y
