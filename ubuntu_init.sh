29dca54e694ca24383501c34eafd301846c97d2e
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2024-05-15 21:44:10 +0200

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,18 +1,18 @@
-# Update
+## Update
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-# Dev Tools
+## Dev Tools
 sudo apt install git wget curl ruby zsh lsd -y
 sudo apt install build-essential -y
 sudo apt install net-tools -y
 sudo apt install python3 python3-pip pipx -y
 
-# Configure SSH
+## Configure SSH
 sudo apt install openssh-server -y
 
-# Install Apps
+## Install Apps
 sudo apt install neofetch -y
 sudo apt install btop -y
 sudo apt install tmux -y
@@ -22,9 +22,9 @@ sudo apt install gnome-shell-extension-manager -y
 sudo apt install gedit -y
 sudo apt install geany -y
 
-# Snap
+## Snap
 
-# Add Flatpack
+## Add Flatpack
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -37,12 +37,12 @@ sudo flatpak install flathub com.jetbrains.PyCharm-Community -y
 sudo flatpak install flathub org.gnome.Builder -y
 sudo flatpak install flathub nz.mega.MEGAsync -y
 
-# CasaOS
+## CasaOS
 curl -fsSL https://get.casaos.io | sudo bash
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-# Brew
+## Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/aiacos/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -50,7 +50,7 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install jesseduffield/lazygit/lazygit
 brew install jesseduffield/lazydocker/lazydocker
 
-# Install Tweak
+## Install Tweak
 pipx install gnome-extensions-cli --system-site-packages
 cd $HOME/.local/share/pipx/venvs/gnome-extensions-cli/bin
 
@@ -66,10 +66,10 @@ cd $HOME/.local/share/pipx/venvs/gnome-extensions-cli/bin
 
 cd
 
-#gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-#gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 #gsettings set org.gnome.desktop.interface icon-theme 'Numix-Square'
-#gsettings set org.gnome.desktop.interface icon-theme 'Yaru-blue'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-blue'
 
 # Dracula theme
 sudo apt-get install dconf-cli -y
@@ -93,8 +93,7 @@ cd shell
 make local-install
 cd 
 
-# Configure ZSH
-# Terminal Setup with Prezto
+## Configure ZSH with Prezto
 # Nerd Fonts
 (curl -Lo "~/.fonts/MesloLGS NF Regular.ttf" "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf") &> /dev/null
 # Reset cache font
@@ -120,29 +119,10 @@ sed -i "s/zstyle ':prezto:module:prompt' theme 'sorin'/zstyle ':prezto:module:pr
 # Configure p10k
 #p10k configure # Should start on new shell
 
-# Grub
+## Grub
 cd 
 cd .settings
 git clone https://github.com/vinceliuice/grub2-themes.git
 cd grub2-themes
 sudo ./install.sh -b -t tela
 cd
-
-## Tmux
-## Environment setup
-tee -a ~/.zshrc << EOF
-# Startup tmux admin console
-session="init"
-
-# Check if the session exists, discarding output
-# We can check $? for the exit status (zero for success, non-zero for failure)
-tmux has-session -t $session 2>/dev/null
-
-if [ $? != 0 ]; then
-  # Set up your session
-  tmux new-session -s 'init' 'btop'\; split-window -h -p 40 \; split-window -v -p 50 \; select-pane -t 1 \; send-keys 'neofetch' C-m \; select-pane -t 2 \; send-keys 'clear' C-m \;
-fi
-
-# tmux attach-session -t $session
-# tmux kill-session
-EOF
