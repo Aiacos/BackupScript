82ce59fa7123dedca143fae7d92f311258d37e9c
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2024-08-12 12:51:29 +0200

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -55,7 +55,7 @@ brew install jesseduffield/lazydocker/lazydocker
 sudo apt install neovim -y
 
 # Dependencies
-sudo apt install npm nodejs cargo ripgrep -y  
+sudo apt install npm nodejs cargo ripgrep fd-find clang clangd -y  
 sudo apt install pipx python3-full python3-pynvim python3-ply -y  
 cargo install tree-sitter-cli
 brew install bottom
