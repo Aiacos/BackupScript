3a0aba5f98d49a0d393e63c919a65f940ecce914
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2024-08-23 16:18:08 +0200

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -63,6 +63,7 @@ brew install zsh-history-substring-search
 brew install atuin
 brew install dust
 brew install jstkdng/programs/ueberzugpp
+brew install yazi
 
 ## Neovim setup
 sudo apt install neovim -y
