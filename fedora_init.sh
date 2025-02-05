8a77c5ab4479d6d9854a79e32ef8db48bd21ed55
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2025-02-05 21:34:38 +0100

    Update fedora_init.sh

--- fedora_init.sh
+++ fedora_init.sh
@@ -100,9 +100,9 @@ rm -rf ~/.config/nvim/.git
 nvim +q
 curl -o ~/.config/nvim/lua/community.lua https://raw.githubusercontent.com/Aiacos/AstroNvim_Config/refs/heads/master/community.lua 
 nvim --headless "+MasonInstall ruff-lsp" +q  
-nvim --headless "+MasonInstall pylama" +q  
 nvim --headless "+MasonInstall pylint" +q
 nvim --headless "+MasonInstall pyment" +q
+# nvim --headless "+MasonInstall pylama" +q  
 
 # Configure ZSH
 sudo dnf install git wget curl ruby ruby-devel zsh util-linux-user redhat-rpm-config gcc gcc-c++ make -y
