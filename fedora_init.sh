ee6b1bced5ae9d9ad57614212d712619124c4f72
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2025-04-20 11:09:59 +0200

    Update fedora_init.sh

--- fedora_init.sh
+++ fedora_init.sh
@@ -191,13 +191,13 @@ sudo snap install spotify --classic
 sudo dnf install gnome-tweaks -y
 pip3 install --user gnome-extensions-cli
 
-#gnome-extensions-cli install arcmenu@arcmenu.com
-gnome-extensions-cli install rocketbar@chepkun.github.com
-#gnome-extensions-cli install tiling-assistant@leleat-on-github
-gnome-extensions-cli install trayIconsReloaded@selfmade.pl
-gnome-extensions-cli install workspace-indicator@gnome-shell-extensions.gcampax.github.com
-gnome-extensions-cli install tophat@fflewddur.github.io
-gnome-extensions-cli install blur-my-shell@aunetx
+#gnome-extensions install arcmenu@arcmenu.com
+gnome-extensions install rocketbar@chepkun.github.com
+#gnome-extensions install tiling-assistant@leleat-on-github
+gnome-extensions install trayIconsReloaded@selfmade.pl
+gnome-extensions install workspace-indicator@gnome-shell-extensions.gcampax.github.com
+gnome-extensions install tophat@fflewddur.github.io
+gnome-extensions install blur-my-shell@aunetx
 
 cd
 
