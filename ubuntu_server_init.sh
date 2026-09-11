02e43478d6ae48332ec7a5d17d25dbe5a9329d27
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2026-09-11 13:08:30 +0200

    Revise Docker installation steps in init script
    Updated Docker installation process to use keyrings and new repository format.

--- ubuntu_server_init.sh
+++ ubuntu_server_init.sh
@@ -34,15 +34,25 @@ ranger --copy-config=all
 #sudo usermod -aG docker $USER
 
 ## Install Docker
+# Add Docker's official GPG key:
 sudo apt update
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
 sudo apt update
-apt-cache policy docker-ce
-sudo apt -y install docker-ce
-sudo chmod 666 /var/run/docker.sock
-docker ps
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
