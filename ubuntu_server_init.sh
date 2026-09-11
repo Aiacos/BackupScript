83b20db60c27f43b2841fa336464f3b9fadc218e
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2026-09-11 13:31:48 +0200

    Refactor Docker installation to use Brew
    Removed Docker installation steps for Debian and added Docker installation via Brew.

--- ubuntu_server_init.sh
+++ ubuntu_server_init.sh
@@ -33,27 +33,6 @@ ranger --copy-config=all
 #sudo groupadd docker
 #sudo usermod -aG docker $USER
 
-## Install Docker
-# Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/debian
-Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-Components: stable
-Architectures: $(dpkg --print-architecture)
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
 ## Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.zshrc
@@ -66,6 +45,8 @@ brew trust jesseduffield/lazydocker
 brew install zellij
 brew install jesseduffield/lazygit/lazygit
 brew install jesseduffield/lazydocker/lazydocker
+brew install docker
+brew install docker-compose
 brew install zsh-history-substring-search
 brew install atuin
 brew install dust
