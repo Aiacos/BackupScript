6ed3bfb43cc2baf04645ad0d0149c463037f0a19
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2026-09-11 13:06:29 +0200

    Update ubuntu_server_init.sh for Docker setup
    Comment out CasaOS installation commands and add Docker installation steps.

--- ubuntu_server_init.sh
+++ ubuntu_server_init.sh
@@ -29,9 +29,25 @@ ranger --cmd=quit!
 ranger --copy-config=all
 
 ## CasaOS
-curl -fsSL https://get.casaos.io | sudo bash
-sudo groupadd docker
-sudo usermod -aG docker $USER
+#curl -fsSL https://get.casaos.io | sudo bash
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
+
+## Install Docker
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt -y install docker-ce
+sudo chmod 666 /var/run/docker.sock
+docker ps
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 ## Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
