35358cc46c67e1d6af3d9ab9ebfa3ef740d0ff7a
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2026-09-11 13:03:26 +0200

    Update ubuntu_server_init.sh

--- ubuntu_server_init.sh
+++ ubuntu_server_init.sh
@@ -23,8 +23,6 @@ sudo apt install chafa -y
 sudo apt install cmatrix -y
 sudo apt install ncdu -y
 sudo apt install timewarrior -y
-sudo apt install gnome-tweaks -y
-sudo apt install gnome-shell-extension-manager -y
 
 sudo apt install caca-utils highlight atool w3m poppler-utils mediainfo -y
 ranger --cmd=quit!
